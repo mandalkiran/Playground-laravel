6ddbfc82fad230c4246ba6533d232a4266f231c2
mandalkiran <karmamandal@gmail.com>
2024-03-15 17:00:43 +0545

    feat: added a test route

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/test', function () {
+    return "Bon Jour....";
+});
+
 require __DIR__.'/auth.php';
