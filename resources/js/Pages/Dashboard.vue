6c5009bd056b79199d2ddf08d78d7cc9e7546eb8
mandalkiran <karmamandal@gmail.com>
2024-04-14 12:03:51 +0545

    feat: reverb in action

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,25 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { router } from '@inertiajs/vue3'
+import {ref, onMounted} from "vue";
+
+const messages = ref([]);
+const data = ref("");
+const submit = () => {
+    router.post('/chat', {
+        'data': data.value
+    })
+    data.value = "";
+}
+
+onMounted(()=> {
+    Echo.channel('cleavr')
+        .listen('MessageEvent', (event) => {
+           messages.value.push(event.message);
+        });
+})
+
 </script>
 
 <template>
@@ -14,7 +33,15 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <ul class="p-6 text-gray-900">
+                        <li v-for="msg in messages">{{msg}}</li>
+                    </ul>
+                    <form  @submit.prevent="submit">
+                        <div class="flex">
+                            <input class="flex-1" v-model="data"/>
+                            <button type="submit" class="mx-3">Send</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
