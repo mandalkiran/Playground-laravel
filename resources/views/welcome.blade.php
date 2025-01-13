78fd36b657c3e9dac5ac52aa5002af4b5e3358c2
Kiran Mandal <thekiranmandal@gmail.com>
2025-01-13 17:04:26 +0545

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -163,7 +163,8 @@
                     </main>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) ----
+                        <p>{{ config('app.variable_name') }}</p>
                     </footer>
                 </div>
             </div>
