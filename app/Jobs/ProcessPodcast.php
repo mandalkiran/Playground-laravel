c719175847890a0f485faec6b523c98e9b8787c6
Kiran Mandal <kiranmandal@lftechnology.com>
2025-01-17 18:31:56 +0545

    redis files

---
+++ app/Jobs/ProcessPodcast.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Jobs;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+class ProcessPodcast implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public $podcast)
+    {
+        Log::info("I am on constructor",$this->podcast);
+    }
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+          Log::info("I am launched",$this->podcast);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
+use App\Jobs\ProcessPodcast;
+
+
+Route::get('/redis-test', function (Request $request) {
+    Redis::set('key', $request->get('query'));
+    $value = Redis::get('key');
+    ProcessPodcast::dispatch(['key' => $value]);
+    dd($value);
+});
+
 
 Route::get('/', function () {
     return view('welcome');
